536049e4fb1eb0f732046eb56dda74ce74d3f84e
Shad Holland <shad.holland@bestbuy.com>
2018-06-19 15:28:34 -0500

    Added test for getMembers()

--- src/components/memberLayout/test.jsx
+++ src/components/memberLayout/test.jsx
@@ -24,10 +24,10 @@ describe('MemberLayout', () => {
 
 describe('getData()', () => {
     beforeEach(() => {
-        const mockData = []
+        const mockData = [];
         const response = {
             json: jest.fn(() => mockData)
-        }
+        };
         global.fetch = jest.fn(() => new Promise(resolve => resolve(response)));
     });
 
@@ -36,7 +36,7 @@ describe('getData()', () => {
     });
 
     it('should return empty data obj', async () => {
-        const mockData = []
+        const mockData = [];
 
         const memberLayout = shallow(
             <MemberLayout />
@@ -44,6 +44,37 @@ describe('getData()', () => {
 
         const data = await memberLayout.instance().getData();
 
-        expect(data).toEqual(mockData)
+        expect(data).toEqual(mockData);
+    });
+});
+
+describe('getMembers()', () => {
+    beforeEach(() => {
+        const mockResponse = {
+            json: jest.fn()
+        };
+        global.fetch = jest.fn(() => new Promise(resolve => resolve(mockResponse)));
+    });
+
+    afterEach(() => {
+        global.fetch = null;
+    });
+
+    it('should return empty data obj', async () => {
+        const mockData = [
+            'member1',
+            'member2'
+        ];
+
+        const memberLayout = shallow(
+            <MemberLayout />
+        );
+        memberLayout.instance().getData = jest.fn(() => new Promise(resolve => resolve(['member1', 'member2'])));
+        memberLayout.update();
+
+        await memberLayout.instance().getMembers('http://www.someurl.com');
+
+        expect(memberLayout.instance().getData).toBeCalledWith('http://www.someurl.com');
+        expect(memberLayout.instance().state.members).toEqual(mockData);
     });
 });
