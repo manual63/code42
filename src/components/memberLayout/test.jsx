817c743a877628e94b657041001ee97d5efa1e87
Shad Holland <shad.holland@bestbuy.com>
2018-06-19 08:52:10 -0500

    Updating tests for MemberLayout to test getMembers()

--- src/components/memberLayout/test.jsx
+++ src/components/memberLayout/test.jsx
@@ -1,12 +1,55 @@
 import React from 'react';
 import MemberLayout from './index';
 
-global.fetch = jest.fn(() => new Promise(resolve => resolve()));
+describe('MemberLayout', () => {
+    beforeEach(() => {
+        global.fetch = jest.fn(() => new Promise(resolve => resolve()));
+    });
 
-test('MemberLayout should match snapshot', () => {
-    const memberLayout = shallow(
-        <MemberLayout />
-    );
-    expect(memberLayout).toMatchSnapshot();
+    afterEach(() => {
+        global.fetch = null;
+    });
+
+    it('should match snapshot', () => {
+        const memberLayout = shallow(
+            <MemberLayout />
+        );
+
+        expect(memberLayout).toMatchSnapshot();
+    });
+});
+
+describe('GetMembers()', () => {
+    beforeEach(() => {
+        const memberList = [
+            'member1',
+            'member2',
+            'member3'
+        ];
+        const response = {
+            json: jest.fn(() => memberList)
+        }
+        global.fetch = jest.fn(() => new Promise(resolve => resolve(response)));
+    });
+
+    afterEach(() => {
+        global.fetch = null;
+    });
+
+    it('should return list of members', async () => {
+        const memberList = [
+            'member1',
+            'member2',
+            'member3'
+        ];
+
+        const memberLayout = shallow(
+            <MemberLayout />
+        );
+
+        const members = await memberLayout.instance().getMembers();
+
+        expect(members).toEqual(memberList)
+    });
 });
 
