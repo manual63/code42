5f83269e8ec4da566c6c193a0303d40aa7b9b559
Shad Holland <shad.holland@gmail.com>
2018-06-19 22:32:38 -0500

    Updating tests to be cleaner and added a few more tests

--- src/components/memberLayout/test.jsx
+++ src/components/memberLayout/test.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import MemberLayout from './index';
 
+beforeEach(() => {
+    const mockResponse = {
+        json: jest.fn(() => ['something', 'something else'])
+    };
+    global.fetch = jest.fn(() => new Promise(resolve => resolve(mockResponse)));
+});
+
+afterEach(() => {
+    global.fetch = null;
+});
+
 describe('MemberLayout', () => {
-    beforeEach(() => {
-        const mockResponse = {
-            json: jest.fn()
-        }
-        global.fetch = jest.fn(() => new Promise(resolve => resolve(mockResponse)));
-    });
-
-    afterEach(() => {
-        global.fetch = null;
-    });
-
     it('should match snapshot', () => {
         const memberLayout = shallow(
             <MemberLayout />
@@ -23,20 +23,8 @@ describe('MemberLayout', () => {
 });
 
 describe('getData()', () => {
-    beforeEach(() => {
-        const mockData = [];
-        const response = {
-            json: jest.fn(() => mockData)
-        };
-        global.fetch = jest.fn(() => new Promise(resolve => resolve(response)));
-    });
-
-    afterEach(() => {
-        global.fetch = null;
-    });
-
-    it('should return empty data obj', async () => {
-        const mockData = [];
+    it('should return valid array', async () => {
+        const mockData = ['something', 'something else'];
 
         const memberLayout = shallow(
             <MemberLayout />
@@ -49,18 +37,7 @@ describe('getData()', () => {
 });
 
 describe('getMembers()', () => {
-    beforeEach(() => {
-        const mockResponse = {
-            json: jest.fn()
-        };
-        global.fetch = jest.fn(() => new Promise(resolve => resolve(mockResponse)));
-    });
-
-    afterEach(() => {
-        global.fetch = null;
-    });
-
-    it('should return empty data obj', async () => {
+    it('should set member list state', async () => {
         const mockData = [
             'member1',
             'member2'
@@ -76,5 +53,45 @@ describe('getMembers()', () => {
 
         expect(memberLayout.instance().getData).toBeCalledWith('http://www.someurl.com');
         expect(memberLayout.instance().state.members).toEqual(mockData);
+    });
+});
+
+describe('getMemberRepos()', () => {
+    it('should set repo list state', async () => {
+        const mockData = [
+            'repo1',
+            'repo2'
+        ];
+
+        const memberLayout = shallow(
+            <MemberLayout />
+        );
+        memberLayout.instance().getData = jest.fn(() => new Promise(resolve => resolve(['repo1', 'repo2'])));
+        memberLayout.update();
+
+        await memberLayout.instance().getMemberRepos('http://www.someurl.com');
+
+        expect(memberLayout.instance().getData).toBeCalledWith('http://www.someurl.com');
+        expect(memberLayout.instance().state.memberRepoList).toEqual(mockData);
+    });
+});
+
+describe('getMemberDetails()', () => {
+    it('should set member details state', async () => {
+        const mockData = [
+            'repo1',
+            'repo2'
+        ];
+
+        const memberLayout = shallow(
+            <MemberLayout />
+        );
+        memberLayout.instance().getData = jest.fn(() => new Promise(resolve => resolve(['repo1', 'repo2'])));
+        memberLayout.update();
+
+        await memberLayout.instance().getMemberRepos('http://www.someurl.com');
+
+        expect(memberLayout.instance().getData).toBeCalledWith('http://www.someurl.com');
+        expect(memberLayout.instance().state.memberRepoList).toEqual(mockData);
     });
 });
